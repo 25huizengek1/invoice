b924c5e2e1e2261b2e843f02a5f08bf76243146a
25huizengek1 <25huizengek1@gmail.com>
2025-07-18 11:31:31 +0200

    feat: add example.sh

---
+++ example.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env nix
+#!nix develop --command bash
+
+go run cmd/invoice/main.go \
+    generate --from "Dream, Inc." --to "Imagine, Inc." \
+    --item "Rubber Duck" --quantity 1 --rate 25 \
+    --tax 0.13 --discount 0.15 \
+    --note "For debugging purposes."
